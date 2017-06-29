e545be59e715b0d77bc8d02619fb5d6531ee453e
Wh1spr <wh1spr.code@gmail.com>
2017-06-29 14:48:02 +0200

    added a messagehandler
    nothing much

---
+++ src/main/java/wh1spr/thunderbot/admin/AdminMessageHandler.java
@@ -0,0 +1,12 @@
+package wh1spr.thunderbot.admin;
+
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class AdminMessageHandler extends ListenerAdapter {
+
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		
+	}
+}
